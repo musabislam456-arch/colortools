cbc1ca580cd431d09d1e7cb25d09595c90541320
Musab Bin Umair <musabislam456@gmail.com>
2026-09-12 17:00:18 +0500

    fix: correct production domain (colortools.utilix.site)

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next';
 import { BLOG_POSTS } from '@/data/blog-posts';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://colortools.studio';
+  const baseUrl = 'https://colortools.utilix.site';
 
   const blogUrls = BLOG_POSTS.map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
