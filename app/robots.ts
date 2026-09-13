c5d9ea0e0f9af4cb600c4a9b51ca636596176d4f
Musab Bin Umair <musabislam456@gmail.com>
2026-09-13 14:51:11 +0500

    fix(seo): correct sitemap domain to colortools.utilix.site

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://colortools.toolbay.site/sitemap.xml',
+    sitemap: 'https://colortools.utilix.site/sitemap.xml',
   };
 }
